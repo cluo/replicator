e962d6c0f4d2ca2235e497050e2b129deb49401c
Eric Westfall <eawestfall@gmail.com>
2017-03-12 21:18:35 -0400

    Add Nomad leader detection method.

---
+++ api/api.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"regexp"
+)
+
+func findIP(input string) string {
+	numBlock := "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+	regexPattern := numBlock + "\\." + numBlock + "\\." + numBlock + "\\." + numBlock
+
+	regEx := regexp.MustCompile(regexPattern)
+	return regEx.FindString(input)
+}
--- api/nomad.go
+++ api/nomad.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	nomad "github.com/hashicorp/nomad/api"
 )
 
@@ -9,6 +11,7 @@ import (
 type NomadClient interface {
 	AllocationCapacity() (*AllocationCapacity, error)
 	AssignedAllocation() (*AllocationUsed, error)
+	LeaderCheck() bool
 }
 
 // The nomadClient object is a wrapper to the Nomad client provided by the
@@ -68,9 +71,9 @@ func (c *nomadClient) AllocationCapacity() (capacity *AllocationCapacity, err er
 		}
 
 		if (resp.Status == "ready") || (resp.Drain != true) {
-			capacity.CPU += resp.Resources.CPU
-			capacity.MemoryMB += resp.Resources.MemoryMB
-			capacity.DiskMB += resp.Resources.DiskMB
+			capacity.CPU += *resp.Resources.CPU
+			capacity.MemoryMB += *resp.Resources.MemoryMB
+			capacity.DiskMB += *resp.Resources.DiskMB
 		}
 	}
 
@@ -98,10 +101,33 @@ func (c *nomadClient) AssignedAllocation() (capacityUsed *AllocationUsed, err er
 		}
 
 		if (resp.ClientStatus == "running") && (resp.DesiredStatus == "run") {
-			capacityUsed.CPU += resp.Resources.CPU
-			capacityUsed.MemoryMB += resp.Resources.MemoryMB
-			capacityUsed.DiskMB += resp.Resources.DiskMB
+			capacityUsed.CPU += *resp.Resources.CPU
+			capacityUsed.MemoryMB += *resp.Resources.MemoryMB
+			capacityUsed.DiskMB += *resp.Resources.DiskMB
 		}
 	}
 	return capacityUsed, nil
+}
+
+// LeaderCheck determines if the local node has cluster leadership.
+func (c *nomadClient) LeaderCheck() bool {
+	haveLeadership := false
+
+	leader, err := c.nomad.Status().Leader()
+	if (err != nil) || (len(leader) == 0) {
+		fmt.Printf("replicator: failed to identify cluster leader")
+	}
+
+	self, err := c.nomad.Agent().Self()
+	if err != nil {
+		fmt.Printf("replicator: unable to retrieve local agent information")
+	} else {
+		attributes := self["member"]
+
+		if findIP(leader) == attributes["Addr"].(string) {
+			haveLeadership = true
+		}
+	}
+
+	return haveLeadership
 }
