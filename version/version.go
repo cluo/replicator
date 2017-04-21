bda12a987d1077931eef389780ac0dc0ab8f56d4
James Rasell <james.rasell@elsevier.com>
2017-04-21 11:19:30 +0100

    Smarter version details including pre-release details.
    This now allows replicator to log the version of replicator
    including the pre-release currently available making the code
    online clearer.

--- version/version.go
+++ version/version.go
@@ -1,8 +1,21 @@
 package version
 
-const humanVersion = "0.0.1"
+import "fmt"
 
-// GetHumanVersion returns a human readable version of replicator.
-func GetHumanVersion() string {
-	return humanVersion
+// The main version number that is being run at the moment.
+const version = "0.0.1"
+
+// A pre-release marker for the version. If this is "" (empty string)
+// then it means that it is a final release. Otherwise, this is a pre-release
+// such as "dev" (in development), "beta", "rc1", etc.
+const versionPrerelease = "dev"
+
+// Get returns a human readable version of replicator.
+func Get() string {
+
+	if versionPrerelease != "" {
+		return fmt.Sprintf("%s-%s", version, versionPrerelease)
+	}
+
+	return version
 }
